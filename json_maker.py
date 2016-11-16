d6f2d0125d75357c9993dc7bb5d2551e37ffec8e
Shonas301 <shippj@purdue.edu>
2016-11-15 21:53:14 -0500

    Changed data type for json

--- json_maker.py
+++ json_maker.py
@@ -10,8 +10,19 @@ amzn_json = open('AMZN.json', 'w')
 
 reader = csv.DictReader(amzn_csv, fieldNames)
 amzn_json.write('[')
+i = 0
 for row in reader:
-    json.dump(row, amzn_json)
+    if i is 0:
+        i = i + 1
+        continue
+    newRow = {}
+    newRow['Open'] = float(row['Open'])
+    newRow['High'] = float(row['High'])
+    newRow['Close'] = float(row['Close'])
+    newRow['Volume'] = int(row['Volume'])
+    newRow['Adj Close'] = float(row['Adj Close'])
+    newRow['Low'] = float(row['Low'])
+    json.dump(newRow, amzn_json)
     amzn_json.write(',\n')
 
 amzn_json.write(']')
