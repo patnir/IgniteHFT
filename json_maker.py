39202bf8b9e528ed56ff7bbde6f053e64b88c4e9
Shonas301 <shippj@purdue.edu>
2016-11-15 21:28:12 -0500

    made a json out of amzn stock for testing

---
+++ json_maker.py
@@ -0,0 +1,14 @@
+import json
+import csv
+
+amzn = {}
+
+fieldNames = ("Date", "Open", "High", "Low", "Close", "Volume", "Adj Close");
+
+amzn_csv  = open('S&P/AMZN.csv', 'r')
+amzn_json = open('AMZN.json', 'w')
+
+reader = csv.DictReader(amzn_csv, fieldNames)
+for row in reader:
+    json.dump(row, amzn_json)
+    amzn_json.write('\n')
