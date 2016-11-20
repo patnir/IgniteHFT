058cc925375f80e6ac516176f3e59bc5a13ad943
patnir <rahul20patni@gmail.com>
2016-11-20 18:49:10 -0500

    main and more classes

--- Tutorial/HR_VirtualFunctions.cpp
+++ Tutorial/HR_VirtualFunctions.cpp
@@ -1,5 +1,53 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
 class Professor {
     int publications;
     int cur_id;
+    
+    
 };
 
+class Person {
+    string name;
+    int age;
+}
+
+class Student {
+    int marks;
+    int cur_id;
+    
+    
+}
+
+
+int main(){
+
+    int n, val;
+    cin>>n; //The number of objects that is going to be created.
+    Person *per[n];
+
+    for(int i = 0;i < n;i++){
+
+        cin>>val;
+        if(val == 1){
+            // If val is 1 current object is of type Professor
+            per[i] = new Professor;
+
+        }
+        else per[i] = new Student; // Else the current object is of type Student
+
+        per[i]->getdata(); // Get the data from the user.
+
+    }
+
+    for(int i=0;i<n;i++)
+        per[i]->putdata(); // Print the required output for each object.
+
+    return 0;
+
+}
